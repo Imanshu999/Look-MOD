17a7c6fe863128b13e05e19a26f052aece7058d3
Imanshu N <n4062226@gmail.com>
2026-07-13 23:09:20 +0530

    Update data.ts

--- src/data.ts
+++ src/data.ts
@@ -194,7 +194,7 @@ export const APPS_DATA: AppItem[] = [
     description: "Watch millions of videos from around the world and download videos without watermarks.",
     longDescription: "InstaPro is an advanced modified version of the social media giant. It allows you to download any photo, video, or reel directly to your local gallery without watermarks. In addition, it removes all annoying sponsored ads from the main feed, unlocks high-fidelity media upload quality, and adds special privacy controls to view stories anonymously, hide typing status, and read messages without sending read receipts.
       ",
-    downloadUrl: "https://www.mediafire.com/file/gn9tjx0oui36cl4/InstaPro-v15.20-look-mod.vercel.app.apk/file",
+    downloadUrl: "https://files.modapkdl.com/ROOT/All%20Documents%20File/Folder%202/InstaPro-v15.20-APPS2APP.COM.apk",
     videoUrl: "https://youtube.com/watch?v=YXUF8lvSNyU", // Added video URL
     screenshots: [
       "https://play-lh.googleusercontent.com/hZ2Ob6d0MqLse2lIdNOGgl93H5y82uhX4hC6mWDUpdwK8LjXAunweDQZ6Hocy0riv4KX0lTIzKJz6lhRDV_BiHg=h305",
