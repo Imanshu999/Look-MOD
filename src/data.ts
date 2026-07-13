e35e281c05d9ccf3e82210c1bf1aa478c8c55b8c
Imanshu N <n4062226@gmail.com>
2026-07-13 23:13:32 +0530

    Update data.ts

--- src/data.ts
+++ src/data.ts
@@ -192,8 +192,7 @@ export const APPS_DATA: AppItem[] = [
     updatedAt: "05/07/2026",
     icon: "https://apps2app.com/wp-content/uploads/2026/03/Insta-Pro-APK-150x150.png",
     description: "Watch millions of videos from around the world and download videos without watermarks.",
-    longDescription: "InstaPro is an advanced modified version of the social media giant. It allows you to download any photo, video, or reel directly to your local gallery without watermarks. In addition, it removes all annoying sponsored ads from the main feed, unlocks high-fidelity media upload quality, and adds special privacy controls to view stories anonymously, hide typing status, and read messages without sending read receipts.
-      ",
+    longDescription: "InstaPro is an advanced modified version of the social media giant. It allows you to download any photo, video, or reel directly to your local gallery without watermarks. In addition, it removes all annoying sponsored ads from the main feed, unlocks high-fidelity media upload quality, and adds special privacy controls to view stories anonymously, hide typing status, and read messages without sending read receipts.",
     downloadUrl: "https://files.modapkdl.com/ROOT/All%20Documents%20File/Folder%202/InstaPro-v15.20-APPS2APP.COM.apk",
     videoUrl: "https://youtube.com/watch?v=YXUF8lvSNyU", // Added video URL
     screenshots: [
